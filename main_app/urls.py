ce499b1087554321d486b2bd906e48c32d9387fc
Aditi Ohri <didiohri@gmail.com>
2019-12-29 13:34:18 -0600

    add createview for user to add artwork

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('art/', views.art_index, name='index'),
-    path('artwork/<int:art_id>', views.art_detail, name='detail'),
-    path('artwork/create', views.ArtCreate.as_view(), name='art_create'),
+    path('art/<int:art_id>', views.art_detail, name='detail'),
+    path('art/create', views.ArtCreate.as_view(), name='art_create')
 ]
