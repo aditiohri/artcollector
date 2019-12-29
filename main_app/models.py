302abc54d766d4808ccafaf99baa24c3fe2774c5
Aditi Ohri <didiohri@gmail.com>
2019-12-29 15:00:17 -0600

    add exhibition model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.urls import reverse
 
+TYPES_OF_EXHIBITIONS = (
+    ('S', 'Solo'),
+    ('D', 'Duo'),
+    ('G', 'Group'),
+    ('P', 'Permanent'),
+)
+
 class Art(models.Model):
     title = models.CharField(max_length=100)
     artist = models.CharField(max_length=100)
@@ -13,3 +20,17 @@ class Art(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'art_id': self.id})
+
+class Exhibition(models.Model):
+    start_date = models.DateField('Beginning of Exhibition')
+    end_date = models.DateField('Leave blank if on permanent display', blank=True, null=True)
+    venue = models.TextField(max_length=150)
+    show = models.CharField(
+        max_length=1,
+        choices=TYPES_OF_EXHIBITIONS,
+        default=TYPES_OF_EXHIBITIONS[0][0]
+        )
+    art = models.ForeignKey(Art, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.get_show_display()} starting {self.start_date}'
