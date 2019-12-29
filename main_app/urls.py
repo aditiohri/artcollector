926f4f61ac698eccc76ab3413b35131b00143b77
Aditi Ohri <didiohri@gmail.com>
2019-12-29 14:04:30 -0600

    add paths for update and delete functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('art/', views.art_index, name='index'),
     path('art/<int:art_id>', views.art_detail, name='detail'),
-    path('art/create', views.ArtCreate.as_view(), name='art_create')
+    path('art/create', views.ArtCreate.as_view(), name='art_create'),
+    path('art/<int:pk>/update', views.ArtUpdate.as_view(), name='art_update'),
+    path('art/<int:pk>/delete', views.ArtDelete.as_view(), name='art_delete'),
 ]
