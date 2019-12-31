0df18928f21f00c297f1a2daf2120dbafc6cf9ed
Aditi Ohri <didiohri@gmail.com>
2019-12-31 14:44:22 -0600

    add Theme model to admin site

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Art, Exhibition
+from .models import Art, Exhibition, Theme
 
 admin.site.register(Art)
 admin.site.register(Exhibition)
+admin.site.register(Theme)
