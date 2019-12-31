ee5e0f0b14627d73b169052ec444d6b6db6050cc
Aditi Ohri <didiohri@gmail.com>
2019-12-31 14:40:59 -0600

    add routes for Theme classbasedviews

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,4 +10,10 @@ urlpatterns = [
     path('art/<int:pk>/update', views.ArtUpdate.as_view(), name='art_update'),
     path('art/<int:pk>/delete', views.ArtDelete.as_view(), name='art_delete'),
     path('art/<int:art_id>/add_expo', views.add_expo, name='add_expo'),
+    path('art/<int:art_id>/add_expo', views.add_expo, name='add_expo'),
+    path('themes/', views.ThemeList.as_view(), name='theme_index'),
+    path('themes/<int:pk>', views.ThemeDetail.as_view(), name='theme_detail'),
+    path('themes/create/', views.ThemeCreate.as_view(), name='theme_create'),
+    path('themes/<int:pk>/update/', views.ThemeUpdate.as_view(), name='theme_update'),
+    path('themes/<int:pk>/delete/', views.ThemeDelete.as_view(), name='theme_delete'),
 ]
