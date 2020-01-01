59b362e096c98d2b4e9989edbf60e1e1692e42d5
Aditi Ohri <didiohri@gmail.com>
2020-01-01 16:48:24 -0600

    register photo model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Art, Exhibition, Theme
+from .models import Art, Exhibition, Theme, Photo
 
 admin.site.register(Art)
 admin.site.register(Exhibition)
 admin.site.register(Theme)
+admin.site.register(Photo)
