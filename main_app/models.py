91639b906dfe1a29bb0382c4fcdb24eae27ff464
Aditi Ohri <didiohri@gmail.com>
2020-01-01 19:42:52 -0600

    experiment with photo models to troubleshoot photo upload -- not root of problem

--- main_app/models.py
+++ main_app/models.py
@@ -57,4 +57,4 @@ class Photo(models.Model):
     art = models.ForeignKey(Art, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'Photo for art_id: {self.art_id} @{self.url}'
+        return f"Photo for art_id: {self.art_id} @{self.url}"
